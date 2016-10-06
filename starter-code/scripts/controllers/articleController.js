04e4eb66d286573708cbcec85220cd4413a663e0
JPGothro <jpgothro@gmail.com>
2016-10-06 13:35:34 -0700

    Added comments answering what methods are doing and execution path

--- starter-code/scripts/controllers/articleController.js
+++ starter-code/scripts/controllers/articleController.js
@@ -7,7 +7,8 @@
     articleView.index(ctx.articles);
   };
 
-  // COMMENT: What does this method do?  What is it's execution path? It's in the middleware chain for /articles/:id. It gets
+  // COMMENT: What does this method do?  What is it's execution path?
+  // It's in the middleware chain for /articles/:id. It gets
   //called before articlesController.index. Finds an article by
   //the params id and sets it to the context object as ctx.articles
   // This method loads by the id
@@ -21,6 +22,10 @@
   };
 
   // COMMENT: What does this method do?  What is it's execution path?
+  // This method finds articles using the supplied author name and sets it
+  // as the context object as ctx.articles.
+  // It is in the middleware chain for /author/:authorName and gets called
+  // before authorController.index.
   articlesController.loadByAuthor = function(ctx, next) {
     var authorData = function(articlesByAuthor) {
       ctx.articles = articlesByAuthor;
@@ -33,6 +38,10 @@
   };
 
   // COMMENT: What does this method do?  What is it's execution path?
+  // This method finds articles using the supplied category and sets it
+  // as the context object as ctx.articles.
+  // It is in the middleware chain for /category/:categoryName and gets called
+  // before articlesController.index.
   articlesController.loadByCategory = function(ctx, next) {
     var categoryData = function(articlesInCategory) {
       ctx.articles = articlesInCategory;
@@ -43,6 +52,10 @@
   };
 
   // COMMENT: What does this method do?  What is it's execution path?
+  // This method loads all articles and sets them as the context object
+  // as ctx.articles.
+  // It is in the middleware chain for '/' and is called before
+  // articlesController.index.
   articlesController.loadAll = function(ctx, next) {
     var articleData = function(allArticles) {
       ctx.articles = Article.all;
